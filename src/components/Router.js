6cf573cf8813d907bb53d2898ab7261347e0194d
zoz0312 <zoz0312@naver.com>
2020-11-26 20:02:05 +0900

    logout add

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,26 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Auth from 'routes/Auth';
+import Profile from 'routes/Profile';
 import Home from '../routes/Home';
+import Navigation from './Navigation';
 
 const AppRouter = ({ isLoggedIn }) => {
 
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Switch>
         {isLoggedIn ?
           <>
-            <Route exact path="/">
-              <Home />
-            </Route>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/profile" component={Profile} />
+            <Redirect from="*" to="/" />
           </>
-          :<Route exact path="/">
-            <Auth />
-          </Route>
+          :<>
+            <Route exact path="/" component={Auth} />
+            <Redirect from="*" to="/" />
+          </>
         }
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,17 @@
+import { authService } from 'firebaseSetup';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Profile = () => {
+  const history = useHistory();
+  const onLogoutClick = () => {
+    authService.signOut();
+    history.push('/');
+  }
   return (
-    <span>Profile</span>
+    <>
+      <button onClick={onLogoutClick}>Logout</button>
+    </>
   )
 }
 
