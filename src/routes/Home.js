19e4ecd6784ef623bcf024ecf525eb7f48033b70
zoz0312 <zoz0312@naver.com>
2020-11-26 20:09:31 +0900

    home component chage

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+  const [crow, setCrow] = useState('');
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+  const onChange = (event) => {
+    const { target: { value } } = event;
+    setCrow(value);
+  }
   return (
-    <span>Home</span>
+    <div>
+      <span>Home</span>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="까악!"
+          maxLength={120}
+          value={crow}
+          onChange={onChange}
+          />
+        <button>까악하기</button>
+      </form>
+    </div>
   )
 }
 
