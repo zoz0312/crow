274c1fa017642c036b5f3c6463b1a1a3b6135739
zoz0312 <zoz0312@naver.com>
2020-11-26 18:25:15 +0900

    auth form add

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onChange = (event) => {
+    const { target : { name, value } } = event;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <span>Auth</span>
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          name="email"
+          type="text"
+          placeholder="Email"
+          required
+          value={email}
+          onChange={onChange} />
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          onChange={onChange} />
+        <input type="submit" value="Login" />
+      </form>
+      <div>
+        <button>구글로 계속하기</button>
+        <button>깃허브로 계속하기</button>
+      </div>
+    </div>
   )
 }
 
