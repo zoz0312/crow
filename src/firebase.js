3a44d2a1713dd53c964a42d9bdcd3e0f43f08b91
zoz0312 <zoz0312@naver.com>
2020-11-26 17:36:54 +0900

    firebase setup add

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAQHeRBTyW7udvZhEzsIFMN6eVx4_-pju4",
+  authDomain: "crow-93676.firebaseapp.com",
+  databaseURL: "https://crow-93676.firebaseio.com",
+  projectId: "crow-93676",
+  storageBucket: "crow-93676.appspot.com",
+  messagingSenderId: "253996481344",
+  appId: "1:253996481344:web:e6e6d6fad7d5d91dc7d8a5"
+};
+
+export default firebase.initializeApp(firebaseConfig);
