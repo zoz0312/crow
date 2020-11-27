f5c5dc19894f7ac4b3e93cd29acd74ef728783cd
zoz0312 <zoz0312@naver.com>
2020-11-27 11:50:51 +0900

    fontawesome add

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -2,6 +2,8 @@ import AuthForm from 'components/AuthForm';
 import { authService, firebaseInstance } from 'firebaseSetup';
 import React from 'react';
 import { Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faGoogle } from "@fortawesome/free-brands-svg-icons"
 import './Auth.scss'
 import crow from 'image/crow.jpg';
 
@@ -24,8 +26,14 @@ const Auth = () => {
       <Image className="crow-image" src={`${crow}`} roundedCircle />
       <AuthForm />
       <div>
-        <button onClick={onSocialClick} name="google">구글로 계속하기</button>
-        <button onClick={onSocialClick} name="github">깃허브로 계속하기</button>
+        <button onClick={onSocialClick} name="google">
+          <FontAwesomeIcon icon={faGoogle} />
+          구글로 계속하기
+        </button>
+        <button onClick={onSocialClick} name="github">
+          <FontAwesomeIcon icon={faGithub} />
+          깃허브로 계속하기
+        </button>
       </div>
     </div>
   )
