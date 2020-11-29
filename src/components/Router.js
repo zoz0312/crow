9dd4d329e08695a86326410db04d7d1765083043
zoz0312 <zoz0312@naver.com>
2020-11-29 18:38:15 +0900

    배포전 bug fix

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 import Auth from 'routes/Auth';
 import Profile from 'routes/Profile';
 import Home from '../routes/Home';
@@ -7,7 +7,7 @@ import Navigation from './Navigation';
 
 const AppRouter = ({ isLoggedIn, userObject, refreshUser }) => {
   return (
-    <BrowserRouter>
+    <Router>
       {isLoggedIn && <Navigation userObject={userObject} />}
       <Switch>
         {isLoggedIn ? (
@@ -26,9 +26,9 @@ const AppRouter = ({ isLoggedIn, userObject, refreshUser }) => {
             </Route>
           </>
         )}
-        {/* <Redirect from="*" to="/" /> */}
+        <Redirect from="*" to="/" />
       </Switch>
-    </BrowserRouter>
+    </Router>
   )
 }
 
